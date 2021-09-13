f0b343633ab167d2de1c08bb8e9b138504babd06
leaskagen <lea.skagen@gmail.com>
2021-09-13 18:19:38 +0200

    new socket

--- src/test/java/no/kristiania/http/httpClientTest.java
+++ src/test/java/no/kristiania/http/httpClientTest.java
@@ -6,10 +6,4 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class httpClientTest {
 
-    @Test
-    void shouldDoSomething(){
-        assertEquals("rune", httpClientMain.testMethod());
-
-    }
-
 }
