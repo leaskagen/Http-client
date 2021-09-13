27a0a87c9ffc732dd6466e27a8c83c3f83f34042
runejac <69790920+runejac@users.noreply.github.com>
2021-09-13 20:26:59 +0200

    deleted one extra class

--- src/test/java/no/kristiania/http/httpClientTest.java
+++ src/test/java/no/kristiania/http/httpClientTest.java
@@ -8,10 +8,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class httpClientTest {
 
-    @Test
+/*    @Test
     void shouldGetSuccessfulResponseCode() {
         HttpClient client = new HttpClient("httpbin.org", 80, "/html");
         assertEquals(200, client.getStatusCode());
 
-    }
+    }*/
 }
